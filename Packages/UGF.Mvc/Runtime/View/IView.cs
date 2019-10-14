a082a6a88b7a3eac8937ddf47ba0c94bcaec80ed
Alexander <vorobyovvorobyov@gmail.com>
2019-10-14 21:52:20 +0300

    Update dependencies

--- Packages/UGF.Mvc/Runtime/View/IView.cs
+++ Packages/UGF.Mvc/Runtime/View/IView.cs
@@ -1,9 +1,11 @@
+using UGF.Initialize.Runtime;
+
 namespace UGF.Mvc.Runtime.View
 {
     /// <summary>
     /// Represents a view.
     /// </summary>
-    public interface IView
+    public interface IView : IInitialize
     {
     }
 }
